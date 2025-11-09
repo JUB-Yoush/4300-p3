f7e4d0894dc6055028a881088d8688aa24ed85e6
JUB_Yoush <yoush1337@gmail.com>
2025-11-08 21:06:19 -0500

    adfadf

--- src/views/Main.cs
+++ src/views/Main.cs
@@ -29,17 +29,29 @@ public enum Modifiers
 [GlobalClass]
 public partial class Preperation : Resource
 {
+    [Export]
     public string Prompt { get; set; }
+
+    [Export]
     public Decision Answer1 { get; set; }
+
+    [Export]
     public Decision Answer2 { get; set; }
 }
 
 [GlobalClass]
 public partial class Decision : Resource
 {
+    [Export]
     public string Description { get; set; }
+
+    [Export]
     public string Outcome { get; set; }
+
+    [Export]
     public Dictionary<Modifiers, int> Influence { get; set; }
+
+    [Export]
     public Preperation NextPrompt { get; set; }
 }
 
